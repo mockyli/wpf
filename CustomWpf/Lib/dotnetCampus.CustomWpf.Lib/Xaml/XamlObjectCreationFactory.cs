59e200f3b67c8915f36b8bdfbcc5b2c47884785a
lindexi <lindexi_gd@163.com>
2022-05-16 12:23:06 +0800

    加上方便 API 调用的逻辑

---
+++ CustomWpf/Lib/dotnetCampus.CustomWpf.Lib/Xaml/XamlObjectCreationFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotnetCampus.WPF.Xaml
+{
+    public static class XamlObjectCreationFactory
+    {
+        public static void RegisterCreator<T>(Func<T> creator)
+        {
+            System.Xaml.Schema.XamlObjectCreationFactory.RegisterCreator(creator);
+        }
+
+        public static void RegisterCreator(Type type, Func<object> creator)
+        {
+            System.Xaml.Schema.XamlObjectCreationFactory.RegisterCreator(type, creator);
+        }
+
+        public static void RegisterTypeCreator(Func<Type, Func<object>> typeCreator)
+        {
+            System.Xaml.Schema.XamlObjectCreationFactory.RegisterTypeCreator(typeCreator);
+        }
+    }
+}
